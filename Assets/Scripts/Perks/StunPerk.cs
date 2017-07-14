74fbafa41c231e0c544f2aad2efe85c26c3153d6
Vincent Chin <vince.chin.aie@gmail.com>
2017-07-14 14:02:27 +1000

    created 25% chance stun perk

---
+++ Assets/Scripts/Perks/StunPerk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunPerk : PerkUpgrader
+{
+    private Player m_player;
+
+    public void Start()
+    {
+        m_player = GameObject.FindObjectOfType<Player>();
+
+    }
+
+    public override void upgrade()
+    {
+        if (m_player != null)
+        {
+            m_player.m_hasStunPerk = true;
+        }
+
+    }
+}
