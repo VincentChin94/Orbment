dacbe0ea1527455b3dc78e8c2fb6ae12c53d8f1e
AIE\s171728 <vince.chin.aie@gmail.com>
2017-07-26 17:03:06 +1000

    added shockwave prototype sphere to fire explosion

---
+++ Assets/Scripts/FX/Expand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expand : MonoBehaviour
+{
+    public float m_scaleRate = 0.1f;
+    //public float m_duration = 0.2f;
+    //private float m_timer = 0.0f;
+    //private float m_ratio = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+        this.transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+
+        this.transform.localScale += Vector3.one  * m_scaleRate;
+
+
+    }
+}
---
+++ Assets/Scripts/FX/FadeAlpha.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAlpha : MonoBehaviour
+{
+    private Renderer m_renderer;
+    public float m_duration = 0.5f;
+    private float m_timer = 0.0f;
+    public float m_ratio = 0.0f;
+    private Material m_material;
+    // Use this for initialization
+    void Start()
+    {
+        m_renderer = this.GetComponent<Renderer>();
+        if (m_renderer != null)
+        {
+            m_material = m_renderer.material;
+        }
+    }
+
+    void OnEnable()
+    {
+        if(m_material != null)
+        {
+            m_material.SetColor("_Albedo", new Color(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_ratio = m_timer / m_duration;
+
+        if (m_material != null)
+        {
+            m_material.SetColor("_Albedo", new Color(1.0f, 1.0f, 1.0f,  m_ratio - 1.0f));
+        }
+        
+        m_timer += Time.deltaTime;
+
+        if(m_timer >= m_duration)
+        {
+            m_timer = 0.0f;
+        }
+    }
+}
