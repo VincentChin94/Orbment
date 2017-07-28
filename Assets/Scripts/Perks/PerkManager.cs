a5e8b494d03cd9fd9d73599f579214d49a8752b4
AIE\s171728 <vince.chin.aie@gmail.com>
2017-07-28 12:15:20 +1000

    trying to smooth slow mo

--- Assets/Scripts/Perks/PerkManager.cs
+++ Assets/Scripts/Perks/PerkManager.cs
@@ -76,12 +76,14 @@ public class PerkManager : MonoBehaviour
                 m_levelUpUI.showUI();
                 m_UpgradeAvailableText.SetActive(false);
                 Time.timeScale = 0.5f;
+                Time.fixedDeltaTime = 0.02f * Time.timeScale;
             }
             else
             {
                 m_levelUpUI.hideUI();
                 m_UpgradeAvailableText.SetActive(true);
                 Time.timeScale = 1.0f;
+                Time.fixedDeltaTime = 0.02f;
             }
 
         }
